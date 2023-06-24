e1f8797018c4d66e4ae6fee779f5440ff44a814d
Ty <ty.cumby@gmail.com>
2023-06-24 11:41:51 -0600

    Update fft samples to compare against FFTW3 instead of 2

--- tests/fft_tests/tmpl_fft_bluestein_cdouble_time_test.c
+++ tests/fft_tests/tmpl_fft_bluestein_cdouble_time_test.c
@@ -3,7 +3,7 @@
 #include <libtmpl/include/tmpl_math.h>
 #include <libtmpl/include/tmpl_complex.h>
 #include <libtmpl/include/tmpl_fft.h>
-#include <fftw.h>
+#include <fftw3.h>
 
 int main(void)
 {
@@ -12,8 +12,8 @@ int main(void)
     const size_t N = (size_t)1000000UL;
     tmpl_ComplexDouble *z0 = malloc(sizeof(*z0) * N);
     tmpl_ComplexDouble *w0 = malloc(sizeof(*w0) * N);
-    fftw_complex *z1 = malloc(sizeof(*z1) * N);
-    fftw_complex *w1 = malloc(sizeof(*w1) * N);
+    fftw_complex *z1 = (fftw_complex*)fftw_malloc(sizeof(*z1) * N);
+    fftw_complex *w1 = (fftw_complex*)fftw_malloc(sizeof(*w1) * N);
     const double start = -20.0;
     const double end = 20.0;
     const double dt = (end - start)/(double)N;
@@ -27,7 +27,8 @@ int main(void)
     {
         x = tmpl_Double_Exp(-t*t);
         z0[n] = tmpl_CDouble_Rect(x, 0.0);
-        z1[n] = (fftw_complex){x, 0.0};
+        z1[n][0] = x;
+        z1[n][1] = 0.0;
 
         t += dt;
     }
@@ -39,16 +40,16 @@ int main(void)
     printf("libtmpl: %f\n", (double)(t2 - t1)/CLOCKS_PER_SEC);
 
     t1 = clock();
-    p = fftw_create_plan(N, FFTW_FORWARD, FFTW_ESTIMATE);
-    fftw_one(p, z1, w1);
+    p = fftw_plan_dft_1d(N, z1, w1, FFTW_FORWARD, FFTW_ESTIMATE);
+    fftw_execute(p);
     t2 = clock();
 
     printf("FFTW:    %f\n", (double)(t2 - t1)/CLOCKS_PER_SEC);
 
     for (n = (size_t)0; n < N; ++n)
     {
-        dx = w0[n].dat[0] - w1[n].re;
-        dy = w0[n].dat[1] - w1[n].im;
+        dx = w0[n].dat[0] - w1[n][0];
+        dy = w0[n].dat[1] - w1[n][1];
 
         tmp = tmpl_Double_Hypot(dx, dy);
 
@@ -63,8 +64,9 @@ int main(void)
     printf("Max Abs Error: %e\n", max);
     printf("RMS Abs Error: %e\n", rms);
 
-    free(z0);
-    free(z1);
+    fftw_destroy_plan(p);
+    fftw_free(z0);
+    fftw_free(z1);
     free(w0);
     free(w1);
 }
